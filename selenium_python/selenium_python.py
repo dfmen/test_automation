440dcae692c93b167c0d1e7686fa84e11a23fc83
Darek-2 <diego.muen@gmail.com>
2023-01-15 21:07:26 -0500

    Trying differents selectors

--- selenium_python/selenium_python.py
+++ selenium_python/selenium_python.py
@@ -1,35 +1,49 @@
 import unittest
-from pyunitreport import HTMLTestRunner
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from helpers import EXECUTION_LINK
 
 class HelloWorld(unittest.TestCase):
-    #Initializing webdriver as driver
-    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
     #Lunch browser
-    @classmethod
-    def setUpClass(cls):
-        cls.driver.maximize_window()
-        cls.driver.implicitly_wait(10)
-
-    #Open web_page
-    def test_hello_world(self):
-        self.driver.get(EXECUTION_LINK)
-
-    #Click button "Ingresar"
-    def test_login(self):
-        button_login = self.driver.find_element(By.XPATH, '//*[@id="Header-v2"]/nav/div[5]/div/a')
-        button_login.click()
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        driver.get(EXECUTION_LINK)
+        driver.maximize_window()
+        driver.implicitly_wait(15)
     
+    #Open web_page
+    def test_search_text_field(self):
+        search_field = self.driver.find_element(By.ID, "search")
+    
+    #Test 1
+    def test_search_text_field_by_name(self):
+        search_field = self.driver.find_element(By.NAME, "q") 
+
+    #Test 1
+    def test_search_text_field_by_class_name(self):
+        search_field = self.driver.find_element(By.CLASS_NAME, "input-text") 
+
+    #Test, to check if a button is enable
+    def test_search_button_enable(self):
+        button = self.driver.find_element(By.CLASS_NAME, "button")
+        button.is_enabled()
+
+    def test_count_of_promo_banner_images(self):
+        banner_list = self.driver.find_element(By.CLASS_NAME, "promos")
+        banners = banner_list.find_elements(By.TAG_NAME, 'img')
+        self.assertEqual(3, len(banners))
+
+    def test_if_banners_equals_to_three(self):
+        banners = self.driver.find_elements(By.XPATH, '//*[@id="top"]/body/div/div[2]/div[2]/div/div/div[2]/ul')
+
+    def test_shopping_cart(self):
+        shopping_cart = self.driver.find_element(By.CSS_SELECTOR, "div.header-minicart span.icon")
+
     #Close browser
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.close()
-        cls.driver.quit()
+    def tearDown(self):
+        #self.driver.close()
+        self.driver.quit()
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2,testRunner=HTMLTestRunner(output='REPORTS', report_name='hello-world-REPORT'))
+    unittest.main(verbosity=2)
